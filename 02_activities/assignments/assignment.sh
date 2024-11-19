a38a4f3689ecc05b723cdbec30d788564df6bb29
Simeon Wong <dtxe@users.noreply.github.com>
2024-11-18 23:00:28 -0500

    download fresh autograder script from repo, renumber questions correctly

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,17 +24,17 @@ unzip rawdata.zip
 
 # 2. Move the ./rawdata directory to ./data/raw
 
-# 4. List the contents of the ./data/raw directory
+# 3. List the contents of the ./data/raw directory
 
-# 5. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+# 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
-# 6. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+# 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
-# 7. Repeat the above step for user logs and event logs
+# 6. Repeat the above step for user logs and event logs
 
-# 8. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-# 9. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
 
 
